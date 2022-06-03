9ddf581b2b79ea3054e310409a004cf5e162e2ff
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-06-03 23:26:33 +0200

    SQL Script gemaakt

---
+++ DB-Scripts/script.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS `nftshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `nftshop`;
+
+/* Producten tabel */
+
+CREATE TABLE `nftshop_products` (
+  `id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `img_src` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `nftshop_products` (`id`, `name`, `img_src`, `price`) VALUES
+(1, 'De gevreesde tomaat', 'tomaat.gif', '30.20'),
+(2, 'Goomba', 'goomba.gif', '249.99');
+
+ALTER TABLE `nftshop_products`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `nftshop_products`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
