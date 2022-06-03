ec64029e11fcf860875f579f42e5399fe09d16ca
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-06-03 12:01:31 +0200

    Start of website

---
+++ Includes/navbar.php
@@ -0,0 +1,31 @@
+<style>
+    <?php include '../Styling/navbar.css'; ?>
+</style>
+
+<ul class="navbar">
+    <li>
+        <a href="home.php">
+            Home
+        </a>
+    </li>
+    <li>
+        <a href="products.php">
+            Products
+        </a>
+    </li>
+    <li>
+        <a href="announcements.php">
+            Announcements
+        </a>
+    </li>
+    <li>
+        <a href="documents.php">
+            Documents
+        </a>
+    </li>
+    <li>
+        <a href="login.php">
+            Log in
+        </a>
+    </li>
+</ul>
