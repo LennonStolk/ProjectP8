a70b8d605261c36f3744bfbafc4548e8e4d57091
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-06-07 01:56:44 +0200

    Minimale announcements pagina

--- Pages/announcements.php
+++ Pages/announcements.php
@@ -20,5 +20,25 @@
             border-bottom: solid rgb(27, 90, 86) 2px;
         }
     </style>
+    <section class="main">
+        <section class="products">
+            <?php 
+                // Get products based on parameter
+                include_once "../Includes/db.php";
+                $sql = 'SELECT * FROM nftshop_announcements';
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                $announcements = $stmt->fetchAll();
+
+                // Display products
+                foreach ($announcements as $announcement) {
+                    echo "<div style='border: 1px solid black'>
+                            <p>{$announcement["author"]}</p>
+                            <p>{$announcement["content"]}</p>
+                        </div>";
+                }
+            ?>
+        </section>
+    </section>
 </body>
 </html>
